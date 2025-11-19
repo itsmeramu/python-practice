3d8f3507d92a0db0506cbb2d25b408baf7be15e3
itsmeramu <113182168+itsmeramu@users.noreply.github.com>
2025-11-19 21:40:13 +0530

    updated day1.py file

--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+# Simple program to get user's name and age, and determine if they are a minor or adult
 name = (input("Enter your name: "))
 age = float(input("Enter your age: "))
 if age < 18:
@@ -5,3 +6,12 @@ if age < 18:
 else:
     print("You are an adult.")
 print(f"Hello, {name}! You are {age} years old.")
+
+# Simple program modulus operation
+print(100%2)
+print(46%3)
+
+# Check if a number is even or odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
